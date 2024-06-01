d331511ca9a6d69e15c7f34aad07c4188b76731c
kwrona05 <kwrona05@gmail.com>
2024-06-01 08:02:10 +0200

    component 3

---
+++ React-practice/src/Message.tsx
@@ -0,0 +1,12 @@
+function Component() {
+    return (
+        <div>
+            <ul>
+                <li>It works</li>
+                <li>It works</li>
+                <li>It works</li>
+            </ul>
+        </div>
+    )
+}
+export default Component
