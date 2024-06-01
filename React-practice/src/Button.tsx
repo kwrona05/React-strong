85de9c215583fe0ba7725540fbf8d27374352372
kwrona05 <kwrona05@gmail.com>
2024-06-01 08:01:56 +0200

    component

---
+++ React-practice/src/Button.tsx
@@ -0,0 +1,11 @@
+
+function GroupList() {
+    return (
+        <ul className="list">
+            <li className="list-item">First Element</li>
+            <li className="list-item">Second Element</li>
+        </ul>
+    )
+}
+
+export default GroupList
