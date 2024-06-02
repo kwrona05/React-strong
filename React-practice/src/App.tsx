965f2e211f4b3b85d4930421ae6dfdfd0845b0aa
kwrona05 <kwrona05@gmail.com>
2024-06-02 11:47:29 +0200

    Create auto save user details

--- React-practice/src/App.tsx
+++ React-practice/src/App.tsx
@@ -1,11 +1,50 @@
-import GroupList from "./Button"
-import Header from "./h1"
+import { useState } from "react";
 
-function App() {
-  return <div>
-  <Header />
-  <GroupList></GroupList>
-  </div>
+function UserDetails() {
+  const [name, setName] = useState("");
+  const [surrname, setSurrname] = useState("");
+  const [job, setJob] = useState("");
+
+  const [header, setHeader] = useState("");
+  const [paragraph, setParagraph] = useState("");
+
+  function saveDetails() {
+    setHeader(`${name} ${surrname}`);
+    setParagraph(job);
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          className="nameInput"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter your first name"
+        />
+        <input
+          type="text"
+          className="surrnameInput"
+          value={surrname}
+          onChange={(e) => setSurrname(e.target.value)}
+          placeholder="Enter your surname"
+        />
+        <input
+          type="text"
+          className="jobInput"
+          value={job}
+          onChange={(e) => setJob(e.target.value)}
+          placeholder="What's your profession?"
+        />
+      </div>
+      <button className="Submit" onClick={saveDetails}>
+        Submit
+      </button>
+      <h1 className="nameHeader">{header}</h1>
+      <p className="jobParagraph">{paragraph}</p>
+    </div>
+  );
 }
 
-export default App
+export default UserDetails;
